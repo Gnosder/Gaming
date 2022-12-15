582d8b66cfc4bb387da18fa5d48674d0a041db00
Patrick <patrick.plaisted@gmail.com>
2022-12-15 11:18:07 -0800

    feature complete for simple version

--- GM Stuff/Overland Travel/Overland Travel Simple.py
+++ GM Stuff/Overland Travel/Overland Travel Simple.py
@@ -1,3 +1,6 @@
+# Import Modules
+import random
+
 def main():
 
     # Init lists of tuples
@@ -5,22 +8,22 @@ def main():
     # Terrain, Highwaymod, RoadMod, TracklessMod, navigationDC, ForageDC
     terrainList = [
         ("Desert", 1, .5, .5, 12, 20),
-        ("Forest(Sparse", 1, 1, .5, 14, 14),
-        ("Forest(Medium)", 1, 1, .5, 16, 14),
-        ("Forest(Dense)", 1, 1, .5, 18, 14),
+        ("Forest, Sparse", 1, 1, .5, 14, 14),
+        ("Forest, Medium", 1, 1, .5, 16, 14),
+        ("Forest, Dense", 1, 1, .5, 18, 14),
         ("Hills", 1, .75, .5, 14, 12),
         ("Jungle", 1, .75, .25, 16, 14),
         ("Moor", 1, 1, .75, 14, 16),
         ("Mountains", .75, .75, .5, 16,18),
         ("Plains", 1, 1, .75, 12, 12),
         ("Swamp", 1, .75, .5, 15, 16),
-        ("Tundra, Frozen", 1, .75, .75, 12, 18)
+        ("Tundra", 1, .75, .75, 12, 18)
     ]
 
     # Travel Mode List
     travelModeList = [
         ("Normal", 1),
-        ("Cautious Travel", .75),
+        ("Cautious", .75),
         ("Exploration", .5),
         ("Foraging", .5),
         ("Leading Mount", .75),
@@ -29,14 +32,15 @@ def main():
 
     # Weather Conditions list
     weatherConditionList = [
-        ("None", 1),
+        ("Normal", 1),
         ("Giant Terrain", .75),
-        ("Poor Visibility (Fog, dark", .5),
-        ("Cold or Hot Climate", .75),
-        ("Storm, normal", .75),
+        ("Poor Visibility", .5),
+        ("Cold", .75),
+        ("Hot", .75),
+        ("Storm", .75),
         ("Storm, powerful", .5),
         ("Hurricane", .1),
-        ("Snow cover, Light", .5),
+        ("Snow cover", .5),
         ("Snow cover, Heavy", .25)
     ]
 
@@ -66,8 +70,10 @@ def main():
         print("Pathtype Error")
         return
 
-    # Determine terrain mod: Search list of tuples for item, return correct modifier
+    # Determine terrain mod: Search list of tuples for item, return correct modifier and DC
     terrainMod = [item for item in terrainList if item[0] == terrain][0][pathtype]
+    terrainDC = [item for item in terrainList if item[0] == terrain][0][4]
+    terrainForagingDC = [item for item in terrainList if item[0] == terrain][0][5]
 
 
     # Ask for Weather Condition
@@ -107,26 +113,48 @@ def main():
     partyNavigation = int(input("Navigation score of party guide: "))
     
     # Check for Compass
+    partyVeer = True
     partyCompass = bool(input("Is the guide using a compass? (T/F): "))
-    
-    # Ask for Survival
-    partySurvival = int(input("Highest Survival Score in Party: "))
-    
+    if partyCompass:
+        partyNavigation = partyNavigation +2
+        partyVeer = False
+
     # Ask for Tracking
     partyTracking = int(input("Tracking Score of guide: "))
  
-    # Determine Lost and Veer
+    # Determine Lost and Veer: Navigation check vs DC
     lost = False
+    if random.randint(1,19) + partyNavigation < terrainDC:
+        lost = True
+        if partyVeer:
+            rand = random.randint(1,9)
+            if rand < 5:
+                partyVeerDirection = "left"
+            elif rand >6:
+                partyVeerDirection = "right"
+            else:
+                partyVeerDirection = "straight"
 
     # Determine Encounter
     encounter = False
-    # Encounter 1 in 8
-    # if rand 1 in 8 is 1 encounter = True
-    # Hex Feature 1 in 2
-    # if rand 1 in 2 is 1 encounter = "Landmark Discovered"
+    if travelMode == "Cautious":
+        rand = random.randint(1,12)
+    elif travelMode == "Exploring":
+        rand = random.randint(1,4)
+    else:
+        rand = random.randint(1,8)
+    encounterRoll1 = random.randint(1, 8)
+    encounterRoll2 = str(random.randint(1, 100)) + "%"
+    if rand == 1:
+        encounter = True
+        if random.randint(1,2):
+            encoutnerType = ["Landmark", encounterRoll1]
+        else:
+            encoutnerType = [encounterRoll2, encounterRoll1]
 
     # Determine Distance Traveled
-    distanceTraveled = partySpeed * travelModeMod * weatherConditionMod * terrainMod
+    distanceTraveledRand = random.randint(2,12) + 3
+    distanceTraveled = partySpeed * travelModeMod * weatherConditionMod * terrainMod * distanceTraveledRand
     if distanceTraveled > 12:
         leftHex = True
 
@@ -136,9 +164,16 @@ def main():
     print("Terrain = ", terrain)
     print("Condition = ", weatherCondition)
     print("Lost = ", lost)
+    if lost:
+        print("The party veers ", partyVeerDirection, ".")
     print("Encounter = ", encounter)
-    print("Distance Traveled = ", distanceTraveled + " miles")
+    if encounter:
+        print("Encounter Type: ", encoutnerType[0])
+        print("Encounter Roll: ", encoutnerType[1])
+    print("Distance Traveled = ", distanceTraveled, " miles")
     print("Left hex =", leftHex)
+    if travelMode == "Foraging":
+        print("Foraging DC: ", terrainForagingDC)
 
 main()
 
